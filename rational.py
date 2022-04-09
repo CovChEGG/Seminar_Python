45407620e54a57f78ff05e6ef51f063c9f341002
CovChEGG <CovChEGG@gmail.com>
2022-04-09 17:40:02 +0700

    реализация вывода рациональных чисел с целой частью

--- rational.py
+++ rational.py
@@ -3,23 +3,26 @@ from fractions import Fraction
 
 def calc(a, b, action):
     a = str_frac(a)
-    b = str_fract(b)
+    b = str_frac(b)
+    res = Fraction(1, 1)
     if action == '+':
-        return a + b
+        res = a + b
     elif action == '-':
-        return a - b
+        res = a - b
     elif action == '*':
-        return a * b
+        res = a * b
     elif b == 0:
         print('Деление на 0 не допустимо!!!')
         exit(1)
     else:
         if action == '/':
-            return a / b
+            res = a / b
         elif action == '%':
-            return a % b
+            res = a % b
         elif action == '//':
-            return a // b
+            res = a // b
+
+    return whole_numbers(res)
 
 
 def str_frac(str1):
@@ -34,3 +37,9 @@ def str_frac(str1):
     res = Fraction(lst1[0], lst1[1])
     return res
 
+
+def whole_numbers(x):
+    out = ''
+    tmp = int(x.numerator/x.denominator)
+    x = Fraction(x.numerator%x.denominator, x.denominator)
+    return f'{tmp} {x}'
